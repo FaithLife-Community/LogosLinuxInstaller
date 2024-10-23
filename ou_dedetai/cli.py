7444e1a3ee1c28f6ee6dd59dadce3925847a4b96
Nate Marti <nate_marti@sil.org>
2024-10-23 13:02:57 +0100

    add app param

--- ou_dedetai/cli.py
+++ ou_dedetai/cli.py
@@ -46,7 +46,7 @@ class CLI:
         wine.install_fonts()
 
     def install_icu(self):
-        wine.install_icu_data_files()
+        wine.install_icu_data_files(app=self)
 
     def remove_index_files(self):
         control.remove_all_index_files()
