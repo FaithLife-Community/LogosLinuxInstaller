961fb112d81863cb53739ba48755de3b56b3cd44
Nate Marti <nate_marti@sil.org>
2024-11-14 14:05:13 +0100

    add 'samba' to arch deps

--- ou_dedetai/system.py
+++ ou_dedetai/system.py
@@ -370,7 +370,7 @@ def get_package_manager():
             # config.PACKAGES = "patch wget sed grep cabextract samba glibc samba apparmor libcurl-gnutls winetricks appmenu-gtk-module lib32-libjpeg-turbo wine giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader"  # noqa: E501
             config.PACKAGES = (
                 "fuse2 fuse3 "  # appimages
-                "binutils cabextract wget libwbclient "  # wine
+                "binutils cabextract libwbclient samba wget "  # wine
                 "p7zip "  # winetricks
                 "openjpeg2 libxcomposite libxinerama "  # display
                 "ocl-icd vulkan-icd-loader "  # hardware
