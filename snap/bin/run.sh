a41ada611fc224dc03adfd0ad58260f8d425f990
Nate Marti <nate_marti@sil.org>
2024-11-25 18:17:57 +0100

    update run.sh

--- snap/bin/run.sh
+++ snap/bin/run.sh
@@ -6,20 +6,25 @@ if [[ $SNAP_NAME != oudedetai ]]; then
     exit 1
 fi
 
-# Ensure config file.
-if [[ ! -r ${SNAP_USER_COMMON}/${SNAP_NAME}.json ]]; then
-    cp $SNAP/etc/${SNAP_NAME}.json ${SNAP_USER_COMMON}/${SNAP_NAME}.json
-fi
-
-# Ensure Logos is installed.
-app_exe="$(find "${SNAP_USER_COMMON}/wine64_bottle" -wholename '*Logos/Logos.exe' 2>/dev/null)"
-if [[ -z $app_exe ]]; then
-    oudedetai --install-app -PFK
-    ec=$?
-    if [[ $ec -ne 0 ]]; then
-        exit $ec
+if [[ $# -gt 0 ]]; then
+    # Run oudedetai command directly with given args.
+    oudedetai $@
+else  # run Logos after ensuring installation
+    # Ensure config file.
+    if [[ ! -r ${SNAP_USER_COMMON}/${SNAP_NAME}.json ]]; then
+        cp $SNAP/etc/${SNAP_NAME}.json ${SNAP_USER_COMMON}/${SNAP_NAME}.json
     fi
-fi
 
-# Run Logos.
-oudedetai --run-installed-app
+    # Ensure Logos is installed.
+    app_exe="$(find "${SNAP_USER_COMMON}/wine64_bottle" -wholename '*Logos/Logos.exe' 2>/dev/null)"
+    if [[ -z $app_exe ]]; then
+        oudedetai --install-app -PFK
+        ec=$?
+        if [[ $ec -ne 0 ]]; then
+            exit $ec
+        fi
+    fi
+
+    # Run Logos.
+    oudedetai --run-installed-app
+fi
