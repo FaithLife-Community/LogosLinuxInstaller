4b17e9fca2736175e704985ffe20877245ea3041
Nate Marti <nate_marti@sil.org>
2024-11-27 17:48:01 +0100

    better handline of config file

--- snap/bin/run.sh
+++ snap/bin/run.sh
@@ -1,30 +1,45 @@
 #!/usr/bin/env bash
 
+# Define config variables.
+FLPRODUCT=Logos
+INSTALLDIR="$SNAP_USER_DATA"
+TARGETVERSION=10
+TARGET_RELEASE_VERSION="37.2.0.0012"
+WINE_EXE="${SNAP}/opt/wine-devel/bin/wine64"
+
 # Ensure correct environment.
 if [[ $SNAP_NAME != oudedetai ]]; then
     echo "ERROR: Not running in oudedetai snap environment."
     exit 1
 fi
 
-if [[ $# -gt 0 ]]; then
-    # Run oudedetai command directly with given args.
-    oudedetai $@
-else  # run Logos after ensuring installation
-    # Ensure config file.
-    if [[ ! -r ${SNAP_USER_COMMON}/${SNAP_NAME}.json ]]; then
-        cp $SNAP/etc/${SNAP_NAME}.json ${SNAP_USER_COMMON}/${SNAP_NAME}.json
-    fi
-
-    # Ensure Logos is installed.
-    app_exe="$(find "${SNAP_USER_COMMON}/wine64_bottle" -wholename '*Logos/Logos.exe' 2>/dev/null)"
-    if [[ -z $app_exe ]]; then
-        oudedetai --install-app -PFK
-        ec=$?
-        if [[ $ec -ne 0 ]]; then
-            exit $ec
-        fi
-    fi
-
-    # Run Logos.
-    oudedetai --run-installed-app
+# Ensure config file.
+config_file="${SNAP_NAME}.json"
+tmp=$(mktemp)
+user_config="${SNAP_USER_DATA}/${config_file}"
+if [[ ! -r $user_config ]]; then
+    cp "${SNAP}/etc/${config_file}" "$user_config"
+    jq --arg a "$FLPRODUCT" '.FLPRODUCT = $a' "$user_config" > "$tmp"
+    mv "$tmp" "$user_config"
+    jq --arg a "$INSTALLDIR" '.INSTALLDIR = $a' "$user_config" > "$tmp"
+    mv "$tmp" "$user_config"
+    jq --arg a "$TARGETVERSION" '.TARGETVERSION = $a' "$user_config" > "$tmp"
+    mv "$tmp" "$user_config"
+    jq --arg a "$TARGET_RELEASE_VERSION" '.TARGET_RELEASE_VERSION = $a' "$user_config" > "$tmp"
+    mv "$tmp" "$user_config"
+    jq --arg a "$WINE_EXE" '.WINE_EXE = $a' "$user_config" > "$tmp"
+    mv "$tmp" "$user_config"
 fi
+
+# Ensure Logos is installed.
+app_exe="$(find "${INSTALLDIR}/wine64_bottle" -wholename "*${FLPRODUCT}/${FLPRODUCT}.exe" 2>/dev/null)"
+if [[ -z $app_exe ]]; then
+    oudedetai --install-app -PFK $@
+    ec=$?
+    if [[ $ec -ne 0 ]]; then
+        exit $ec
+    fi
+fi
+
+# Run Logos.
+oudedetai --run-installed-app $@
